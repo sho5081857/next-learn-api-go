8b7d51be18ac2529d73297fbe6f4d9541f2266f5
sho5081857 <masashi713416@gmail.com>
2024-05-20 23:41:07 +0900

    Modified sql file execution process.

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"io"
@@ -35,23 +36,35 @@ func NewDB() *bun.DB {
 		log.Println("Failed to connect to the database:", err)
 	}
 
-	// SQLファイルを開く
-	file, err := os.Open("_tools/first.sql")
+	// テーブルが存在するかどうかを確認するクエリを実行
+	var exists bool
+	err = db.NewSelect().
+		ColumnExpr("EXISTS (SELECT FROM pg_tables WHERE tablename = ?) AS exists", "users").
+		Scan(context.Background(), &exists)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	// SQLファイルの内容を読み込む
-	content, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
-	// SQLファイルの内容をクエリとして実行
-	_, err = db.Exec(string(content))
-	if err != nil {
-		log.Fatal(err)
+	// テーブルが存在しない場合のみ選択されたコードを実行
+	if !exists {
+		// SQLファイルを開く
+		file, err := os.Open("_tools/first.sql")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		// SQLファイルの内容を読み込む
+		content, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// SQLファイルの内容をクエリとして実行
+		_, err = db.Exec(string(content))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	fmt.Println("Connected")
